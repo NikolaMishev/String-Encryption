3cd05720fc55f4e9308edacb1bdd2b0603684f32
NikolaMishev <nmishev91@gmail.com>
2017-03-25 19:45:45 +0200

    Added solution files of the problem

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var result = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentChar = char.Parse(Console.ReadLine());
+
+                var encrypted = Encrypt(currentChar);
+
+                result += encrypted;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static string Encrypt(char currentChar)
+        {
+            var askiiCode = (int)currentChar;
+
+            var firstDigit = askiiCode;
+
+            while (firstDigit >= 10)
+            {
+                firstDigit = firstDigit / 10;
+            }
+
+            var lastDigit = currentChar % 10;
+
+            var firstLast = firstDigit.ToString() + lastDigit.ToString();
+
+            var start = (char)(askiiCode + lastDigit);
+
+            var end = (char)(askiiCode - firstDigit);
+
+            var result = start + firstLast + end;
+
+            return result;
+
+        }
+    }
+}
